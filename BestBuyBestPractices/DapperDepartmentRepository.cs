ec012d9e7f301a87e27cfbf1be1e36483c0587db
Kate Lynn Thomson <katelynn.jaremko@gmail.com>
2025-08-14 13:02:31 -0500

    Exercise 1

---
+++ BestBuyBestPractices/DapperDepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.Json;
+using MySql.Data;
+
+namespace BestBuyBestPractices
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _conn;
+
+        //constructor
+        public DapperDepartmentRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _conn.Query<Department>("SELECT * FROM departments");
+        }
+
+        public void InsertDepartment(string name)
+        {
+            _conn.Execute("INSERT INTO departments (Name) VALUES (@name)", 
+                new { name = name });
+        }
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System; 
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using BestBuyBestPractices;
+using Dapper;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+        string connString = config.GetConnectionString("DefaultConnection");
+
+        IDbConnection conn = new MySqlConnection(connString);
+
+        var departmentRepo = new DapperDepartmentRepository(conn);
+
+        var departments = departmentRepo.GetAllDepartments();
+
+        foreach (var dep in departments)
+        {
+            Console.WriteLine(dep.DepartmentID);
+            Console.WriteLine(dep.Name);
+        }
+    }
+}
